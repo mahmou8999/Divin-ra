69049e3004196a585ea3b638ea0cd9db771dde21
mahmou8999 <mahmoud.rabie2512@gmail.com>
2026-01-18 18:32:52 +0200

    Format Layout.js for consistency

--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import NavBar from "../components/navbar&footer/NavBar";
 import { Outlet } from "react-router";
-import Section1 from "../components/section/Section1";
 import Footer from "../components/navbar&footer/Footer";
 
 function Layout() {
@@ -15,3 +14,4 @@ function Layout() {
 }
 
 export default Layout;
+
