2d81a97dbfbda8079adfec996a56f3853dddf01d
mahmou8999 <mahmoud.rabie2512@gmail.com>
2026-01-18 18:32:11 +0200

    Remove unused imports from index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
-import { BrowserRouter } from "react-router";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import { routes } from "./routes/Routes";
 import { RouterProvider } from "react-router-dom";
